9814fa6207afd179403faafe0dd01a8645aca179
lawilliams713 <lawilliams713@gmail.com>
2019-09-17 17:04:44 -0500

    updates

---
+++ models/index.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const URI = require('../client/config/');
+
+
+mongoose.connect(process.env.MONGODB_URI || URI);
+
+mongoose.connect(
+  URI,
+  { useNewUrlParser: true }
+);
+
+mongoose.connection.on('connected', function () {  
+  console.log('Established mongoose default connection');
+}); 
+
+mongoose.connection.on('error',function (err) {  
+  console.log('Mongoose default connection error: ' + err);
+}); 
+
+mongoose.connection.on('disconnected', function () {  
+  console.log('Mongoose default connection disconnected'); 
+});
